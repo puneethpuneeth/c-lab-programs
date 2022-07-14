dcee8b81bc9f8854ef96da527aa35f72eb2d780b
puneethpuneeth <108451870+puneethpuneeth@users.noreply.github.com>
2022-07-14 12:45:25 +0530

    Add files via upload

--- bubb.c
+++ bubb.c
@@ -1,37 +1,66 @@
+#include<stdio.h>
+int main()
+{
+   int n,i,j,temp;
+   printf("enter the size of the array");
+   scanf("%d",&n);
+   int a[n];
+   //accept the elements into the array
+   printf("enter the elements");
+   for(i=0;i<n;i++)
+   {
+      scanf("%d",&a[i]);
+   }
+      //sort the array
+       for(i=0;i<n-1;i++)
+       {
+          for(j=0;j<n-1-i;j++)
+          {
+            if(a[j]>a[j+1])
+          {
+             temp=a[j];
+             a[j]=a[j+1];
+             a[j+1]=temp;
+           }
+       }
+   }
+          printf("sorted array i=\n");
+          for(i=0;i<n;i++)
+         {
+              printf("%d\t",a[i]);
+         }
+	int key;
+	printf("\nenter the key");
+	scanf("%d",&key);
+          int f=0;
+          int l=n-1;
+             while(f<=l)
+             {
+              int mid=(f+l)/2;
+              if(a[mid]==key)
+              {
+              printf("%d is found at location %d\n",key,mid+1);
+               return 0;
+              }
+              else if(key>a[mid])
+              {
+                f=mid+1;
+              }
+               else
+              {
+                l=mid-1;
+              }
+            }
+              printf("key not found\n");
+		return 1;
+            }
+         
+ 
 
-    #include<stdio.h>    
-     void print(int a[], int n) //function to print array elements  
-        {  
-        int i;  
-        for(i = 0; i < n; i++)    
-        {    
-            printf("%d ",a[i]);    
-        }        
-        }  
-     void bubble(int a[], int n) // function to implement bubble sort  
-     {  
-       int i, j, temp;  
-       for(i = 0; i < n; i++)    
-        {    
-          for(j = i+1; j < n; j++)    
-            {    
-                if(a[j] < a[i])    
-                {    
-                    temp = a[i];    
-                    a[i] = a[j];    
-                    a[j] = temp;     
-                }     
-            }     
-        }     
-     }  
-    void main ()    
-    {    
-        int i, j,temp;     
-        int a[5] = { 10, 35, 32, 13, 26};     
-        int n = sizeof(a)/sizeof(a[0]);   
-        printf("Before sorting array elements are - \n");  
-        print(a, n);  
-        bubble(a, n);  
-        printf("\nAfter sorting array elements are - \n");    
-        print(a, n);  
-    }    
+           
+            
+
+    
+     
+      
+                                                                     
